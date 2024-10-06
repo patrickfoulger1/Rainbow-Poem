4b6e4155d7b72e300bccdab651c16ca7d8d472ff
patrickfoulger1 <patrickfoulger1@gmail.com>
2024-10-06 20:08:07 +0100

    Create rain.js

---
+++ rain.js
@@ -0,0 +1,109 @@
+
+const the_atomic_number_of_Niobium = 41
+const the_runtime_of_The_Thing = 109
+const I_asked_a_group_of_prophets_where_next_my_list_should_go = true
+
+const raindrops_by_jolly = {
+
+1:` I couldn't count the", raindrops,
+    as they fell into the sea"
+    they hurried by in slithers
+    they were too fast for me`,
+
+2:  `I kept a`, list_of_data:[
+    6,'s ', 5,'s and strings',  
+    the_atomic_number_of_Niobium, 
+    the_runtime_of_The_Thing,
+
+    I_asked_a_group_of_prophets_where_next_my_list_should_go ? 
+    48 : `of 50 said "My friend, I do do not know"',
+
+    and it all just seemed so random
+    every 1 and every 0
+    endless streams of binary
+    endless lines of monochrome
+    '
+    ` ,
+    
+    `||||||||||
+    ||||||||||
+    ||||  ||||||||  ||||||
+    |||  ||||  ||||  ||||||
+    ||||  |||||||  |||||||||||
+    ||||||||  |||||  ||||||||
+    ||   |||||||  |||
+    ||||||  |||||  ||||
+    |  ||   |||  |||||||||||||||||||
+    |||       |||||||
+    ||||||             ||||
+    ||||  ||||||||  ||||||
+    |||  ||||  ||||  ||||||
+    ||||       |||||||  |||||||||||
+    ||||||||  ||||
+    |  |||||  |||||||||||||||||||
+    |||||  |||||
+    ||||||||         ||
+    ||||  ||||||  ||||||||
+    ||   |||||||  |||
+    ||||||  |||||  ||||
+    |  |||||  |||||||||||||||||||
+    |||||              |||||
+    |||||||      |||
+    ||||  ||||||||  ||||||
+    |||  |||  |||||||
+    ||||||||  |||||  ||||||||
+    ||   |||||||  |||
+    ||||||  |||||  ||||
+    |  |||||  |||||||||||||||||||
+    `,
+    00101110,
+    00101110, 
+    00101110, 
+],
+
+//I caught A single raindrop
+jar: raindrop(),
+
+//and constructed a machine
+machine: 
+function(input, specimen) {return String.fromCharCode(
+parseInt(input[0].toString()+input[2].toString()),input[5],
+100-3,(input[4]*3)-9,specimen+4,(input[6]*2)+12,(input[5]-
+1),(input[4] * 3) - 2, specimen - 1, specimen, 32, input[6], 
+input[6] + 10, input[4])},
+
+//I'd analyse the water
+
+ microscope: `  0                
+               101
+               010
+              10101
+             0101010
+            101010101
+           10101010101
+          0111011001100
+         110011001100110
+        01100110011001100
+      110011001100110011001
+    1001 00110011001100110011
+  00111  0101010101010101010101
+ 0101   101010101010101010101010
+1010   01011100100010001000100010
+001   100010001000100010001000100
+0010   00010001000100010001000100
+ 00100  001100110011001000100010
+  00100 10001000101010101010101
+    0101010101010101010101010
+      000100100001000100000
+           00000000010`
+
+
+//to find out what it means
+
+};
+
+
+function raindrop(){
+    return   101
+};
+
